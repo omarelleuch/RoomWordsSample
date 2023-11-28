46c3e6ed67213832156b8e2fa63026c65ed38b6c
POST7 PROG1 <post7.prog1@enetcom.rnu.tn>
2023-11-28 10:51:22 +0100

    message

---
+++ app/src/main/java/com/example/roomwordssample/WordViewModel.java
@@ -0,0 +1,28 @@
+package com.example.roomwordssample;
+
+import android.app.Application;
+
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import com.example.roomwordssample.data.WordRepository;
+import com.example.roomwordssample.model.Word;
+
+import java.util.List;
+
+public class WordViewModel extends AndroidViewModel {
+
+    private final WordRepository mRepository;
+
+    private final LiveData<List<Word>> mAllWords;
+
+    public WordViewModel (Application application) {
+        super(application);
+        mRepository = new WordRepository(application);
+        mAllWords = mRepository.getAllWords();
+    }
+
+    LiveData<List<Word>> getAllWords() { return mAllWords; }
+
+    public void insert(Word word) { mRepository.insert(word); }
+}
---
+++ app/src/main/java/com/example/roomwordssample/model/Word.java
@@ -0,0 +1,28 @@
+package com.example.roomwordssample.model;
+
+import androidx.annotation.NonNull;
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "word_table")
+public class Word {
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+    @NonNull
+    @ColumnInfo(name = "word")
+    private String mWord;
+
+    public Word(@NonNull String word) {this.mWord = word;}
+
+    public String getWord(){return this.mWord;}
+}
